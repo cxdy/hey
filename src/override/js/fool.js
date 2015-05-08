ede0197d375f62be51ef04f48fd36dbf25ee42ea
Cody Kaczynski <cxdy@users.noreply.github.com>
2015-05-08 12:45:57 -0400

    Update fool.js

--- src/override/js/fool.js
+++ src/override/js/fool.js
@@ -118,7 +118,7 @@
 				
 				//  make a shutter descend unto the screen
 				shutter: function() {
-					var shutter = body.append('<div id="shutter"><h1 class="time" style="color: white;">christian marasco is a loser</h1></div>').children('#shutter');
+					var shutter = body.append('<div id="shutter" style="background-color: url('http://img3.wikia.nocookie.net/__cb20130914124100/glee/images/e/e2/Funny-gifs-dancing-alone1.gif');"><h1 class="time" style="color: white;">christian marasco is a loser</h1></div>').children('#shutter');
 					
 					shutter.css({
 						position: 'fixed',
@@ -128,7 +128,6 @@
 						bottom: '100%',
 						
 						background: '#000',
-						background-image: url('http://img3.wikia.nocookie.net/__cb20130914124100/glee/images/e/e2/Funny-gifs-dancing-alone1.gif')
 					}).animate({
 						bottom: 0
 					}, 500);
